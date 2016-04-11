4b987cf658fa0ebb98e457a7745c3443b8eefc5c
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-04-11 15:25:28 -0400

    generating products controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.new(
+  name: "Laptop",
+  text: "this is a laptop",
+  price_in_cents: 49.99
+)
